0e2f7ba90af69cd57bbd7fc694e24086b8ee7c2b
edmilson-prog <edmilson@ailainteligente.com>
2026-07-15 21:43:19 -0300

    test: add setPointerCapture polyfill for Radix Select interactions
    Rounds out the scrollIntoView/pointer-capture polyfill block added for
    ComponenteCustoForm's required equipamento Select, matching the same
    set already declared locally in ordem-form.test.tsx.

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -48,6 +48,9 @@ if (!Element.prototype.scrollIntoView) {
 if (!Element.prototype.hasPointerCapture) {
   Element.prototype.hasPointerCapture = () => false;
 }
+if (!Element.prototype.setPointerCapture) {
+  Element.prototype.setPointerCapture = () => {};
+}
 if (!Element.prototype.releasePointerCapture) {
   Element.prototype.releasePointerCapture = () => {};
 }
